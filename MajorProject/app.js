92bba434d6801e13ee8f18ac4bb7425130ec7171
devikapandya1 <devikajoshi2923@gmail.com>
2025-06-27 10:28:22 +0530

    mongoose middleware

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -8,7 +8,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
+
 
 
 app.set("view engine","ejs");
@@ -44,6 +46,17 @@ const validateListing = (req,res,next)=>{
     }
 };
 
+//validate review 
+const validateReview = (req,res,next)=>{
+  let {error} =reviewSchema.validate(req.body);
+    if(error){
+      let errMsg = error.details.map((el)=>el.message).join(",")
+      throw new ExpressError(400,errMsg);
+    }else{
+      next();
+    }
+};
+
 //index route
 app.get("/listings",wrapAsync(async(req,res)=>{
    let allListings = await Listing.find({});
@@ -58,7 +71,7 @@ app.get("/listings/new", (req, res) => {
 //Show Route
 app.get("/listings/:id", wrapAsync(async(req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id);
+  const listing = await Listing.findById(id).populate("reviews");
   res.render("listings/show.ejs", { listing });
 }));
 
@@ -92,6 +105,28 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
   res.redirect("/listings");
 }));
 
+//Reviews
+//Post Review route
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  res.redirect(`/listings/${listing._id}`)
+}));
+
+//delete Review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+  let{id,reviewId} = req.params;
+
+  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/listings/${id}`)
+//
+}))
 
 
 app.all("/*splat",(req, res, next) => {
