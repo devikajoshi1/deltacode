a1ca539ec3793752ec5f23683fa16b57259b3286
devikapandya1 <devikajoshi2923@gmail.com>
2025-07-25 11:44:59 +0530

    lottery gane using states

---
+++ React/lottery-game/src/App.jsx
@@ -0,0 +1,15 @@
+import Lottery from './Lottery'
+import './App.css'
+import TicketNum from './TicketNum'
+import Ticket from './Ticket'
+
+function App() {
+
+  return (
+    <>
+    <Lottery n={3} winningSum={15}/>
+    </>
+  )
+}
+
+export default App
